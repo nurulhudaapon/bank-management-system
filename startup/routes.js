c0da91d8e4533b9cc80514edfeacd9dc1db97ae4
Nurul Huda <nurulhuda859g@gmail.com>
2019-05-09 15:34:10 +0600

    added better admin panel

--- startup/routes.js
+++ startup/routes.js
@@ -4,6 +4,7 @@ const customer = require('../routes/customer');
 const account = require('../routes/account');
 const deposit = require('../routes/deposit');
 const addNew = require('../routes/addNew');
+const statistics = require('../routes/statistics');
 
 module.exports = function (app) {
     // Assigning routes
@@ -12,4 +13,5 @@ module.exports = function (app) {
     app.use('/api/customer', customer);
     app.use('/api/account', account);
     app.use('/api/deposit', deposit);
+    app.use('/api/statistics', statistics);
 }
