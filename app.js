04a2ff67b75cb7df459baaf5a29c6fbc7250824e
Nurul Huda <nurulhuda859g@gmail.com>
2019-05-09 15:41:37 +0600

    bug fix

--- app.js
+++ app.js
@@ -15,11 +15,3 @@ app.use(error);
 const port = process.env.app_port || 8080;
 const host = process.env.app_host || '127.0.0.0';
 app.listen(port, () => console.log('Server running http//:'+host+':'+port));
-
-
-app.get('/set', (req, res) => {
-    process.env.NODE_ENV = req.query.mode;
-    res.send('Mode set to '+ process.env.NODE_ENV);
-    console.log('Mode set to '+ process.env.NODE_ENV);
-    
-})
--- index.js
+++ index.js
@@ -13,14 +13,4 @@ require ('./startup/routes')(app);
 app.use(error);
 
 const port = process.env.PORT || 3000;
-// const host = process.env.HOST || '127.0.0.2'
 app.listen(port, () => console.log('Server started at: http://localhost/'));
-
-
-app.get('/set', (req, res) => {
-    process.env.NODE_ENV = req.query.mode;
-    res.send('Mode set to '+ process.env.NODE_ENV);
-    console.log('Mode set to '+ process.env.NODE_ENV);
-    
-})
-
