7b41b454a3b2bce94e8ca39f18fe50ce35c9844b
Nurul Huda <nurulhuda859g@gmail.com>
2019-05-13 15:24:04 +0600

    enhanced dashboard

--- public/static/js/edit-deposit.js
+++ public/static/js/edit-deposit.js
@@ -1,5 +1,123 @@
 
+// Get the Sidebar
+var mySidebar = document.getElementById("mySidebar");
+
+// Get the DIV with overlay effect
+var overlayBg = document.getElementById("myOverlay");
+
+// Toggle between showing and hiding the sidebar, and add overlay effect
+function w3_open() {
+    if (mySidebar.style.display === 'block') {
+        mySidebar.style.display = 'none';
+        overlayBg.style.display = "none";
+    } else {
+        mySidebar.style.display = 'block';
+        overlayBg.style.display = "block";
+    }
+}
+
+// Close the sidebar with the close button
+function w3_close() {
+    mySidebar.style.display = "none";
+    overlayBg.style.display = "none";
+}
+
+
+
+
+
+
+
+
+
+
+let contentp;
+var customer = document.getElementById('customer');
+var account = document.getElementById('account');
+var deposit = document.getElementById('deposit');
+var total = document.getElementById('total');
+var lastDeposit = document.getElementById('last-deposit-time');
+
+
+
+async function run2() {
+
+    let result = await fetch('/api/statistics');
+    let info = await result.json();
+
+    console.log(info);
+
+    let cdate = Date.now()
+    let ldate = new Date(info.deposits[info.deposits.length - 1].date).getTime();
+
+    function dhm(t) {
+        var cd = 24 * 60 * 60 * 1000,
+            ch = 60 * 60 * 1000,
+            d = Math.floor(t / cd),
+            h = Math.floor((t - d * cd) / ch),
+            m = Math.round((t - d * cd - h * ch) / 60000),
+            pad = function (n) { return n < 10 ? '0' + n : n; };
+        if (m === 60) {
+            h++;
+            m = 0;
+        }
+        if (h === 24) {
+            d++;
+            h = 0;
+        }
+        return [d+' Days '+ pad(h)+' Hours '+ pad(m)+' Minutes'];
+    }
+
+    console.log(dhm(3 * 24 * 60 * 60 * 1000));
+
+    total.innerText = info.total;
+    customer.innerText = info.customer;
+    account.innerText = info.accountCount;
+    deposit.innerText = info.depositCount;
+    // lastDeposit.innerText = dhm(cdate - ldate);
+    // - info.deposits[info.deposits.length - 1].date
+    console.log(info.deposits[info.deposits.length - 1]);
+
+    contentp =document.getElementById('content').innerHTML
+
+}
+run2();
+
+
+function changeTo(c) {
+    document.getElementById('content').innerHTML = `<iframe width="100%" height="1200px" src=/${c}>`
+}
+
+function changeToP() {
+    document.getElementById('content').innerHTML = contentp;
+}
+
 let tr = document.getElementById('tr');
+function changeToD() {
+
+    editDeposit();
+}
+
+// --------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 async function editDeposit(params) {
     let res = await fetch('/api/deposit');
     let json = await res.json();
@@ -7,17 +125,29 @@ async function editDeposit(params) {
     json.forEach((e, i) => {
 
         // list.innerHTML += '<li>'+i+ e.name+'<li>'
+        // tr.innerHTML +=
+        //  `
+        //                     <form id='dpsEdit-${i}' onsubmit='sendDepositData(this)' action='/api/deposit/test'>
+        //                         <input name='_id' hidden value='${e._id}'>
+        //                         <input name='acn' name='acn' readonly value='${e.acn}'>
+        //                         <input name='name' readonly value='${e.name}'>
+        //                         <input name='dBy' class='dpsEdit-${i}' readonly value='${e.dBy}'>
+        //                         <input name='dBy' class='dpsEdit-${i}' readonly value='${e.dTo}'>
+        //                         <input name='dTo' class='dpsEdit-${i}' readonly type='date' value='${e.date.split('T')[0]}'>
+        //                         <input name='amount' class='dpsEdit-${i}' readonly value='${e.amount}'>
+        //                         <input type='button' value='Edit' onclick="removeReadonly('dpsEdit-${i}')">
+        //                         <input type='submit' value='Submit'>
+        //                     </form><br>
+
+        // `
         tr.innerHTML += `
                             <form id='dpsEdit-${i}' onsubmit='sendDepositData(this)' action='/api/deposit/test'>
                                 <input name='_id' hidden value='${e._id}'>
                                 <input name='acn' name='acn' readonly value='${e.acn}'>
                                 <input name='name' readonly value='${e.name}'>
-                                <input name='dBy' class='dpsEdit-${i}' readonly value='${e.dBy}'>
-                                <input name='dBy' class='dpsEdit-${i}' readonly value='${e.dTo}'>
                                 <input name='dTo' class='dpsEdit-${i}' readonly type='date' value='${e.date.split('T')[0]}'>
                                 <input name='amount' class='dpsEdit-${i}' readonly value='${e.amount}'>
-                                <input type='button' value='Edit' onclick="removeReadonly('dpsEdit-${i}')">
-                                <input type='submit' value='Submit'>
+                                <button type='button' onclick="removeReadonly('dpsEdit-${i}')"><i class="fa fa-edit fa-fw"></i></button>
                             </form><br>
 
         `
@@ -25,6 +155,8 @@ async function editDeposit(params) {
 
 
     });
+    document.getElementById('content').innerHTML = document.getElementById('tr').innerHTML;
+
 }
 
 function removeReadonly(className) {
@@ -32,9 +164,10 @@ function removeReadonly(className) {
     for (let i = 0; elements.length > i; i++) {
         console.log(elements);
         elements[i].style.border = '1px solid black';
-        elements[3].focus();
+        elements[1].focus();
 
         elements[i].removeAttribute('readonly');
+        
     }
+    elements.innerHTML +=   `<button type='button'><i class="fa fa-edit fa-fw">fgdf</i></button>` 
 }
-editDeposit();
