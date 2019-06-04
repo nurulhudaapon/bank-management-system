95793d0a88486b14d496e89335318476dce30457
nurulhuda859 <nurulhuda859g@gmail.com>
2019-06-04 20:11:00 +0600

    testing

--- routes/callback.js
+++ routes/callback.js
@@ -6,61 +6,60 @@ router.use(express.json());
 
 router.get('/webhook/facebook', (req, res) => {
     console.log('request recived');
-    
-      // Your verify token. Should be a random string.
-  let VERIFY_TOKEN = "bp"
-    
-  // Parse the query params
-  let mode = req.query['hub.mode'];
-  let token = req.query['hub.verify_token'];
-  let challenge = req.query['hub.challenge'];
-    
-  // Checks if a token and mode is in the query string of the request
-  if (mode && token) {
-  
-    // Checks the mode and token sent is correct
-    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-      
-      // Responds with the challenge token from the request
-      console.log('WEBHOOK_VERIFIED');
-      res.status(200).send(challenge);
-    
-    } else {
-      // Responds with '403 Forbidden' if verify tokens do not match
-      res.status(404).send('Invalid request');     
+
+    // Your verify token. Should be a random string.
+    let VERIFY_TOKEN = "bp"
+
+    // Parse the query params
+    let mode = req.query['hub.mode'];
+    let token = req.query['hub.verify_token'];
+    let challenge = req.query['hub.challenge'];
+
+    // Checks if a token and mode is in the query string of the request
+    if (mode && token) {
+
+        // Checks the mode and token sent is correct
+        if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+
+            // Responds with the challenge token from the request
+            console.log('WEBHOOK_VERIFIED');
+            res.status(200).send(challenge);
+
+        } else {
+            // Responds with '403 Forbidden' if verify tokens do not match
+            res.status(404).send('Invalid request');
+        }
     }
-  }
 });
 
 router.post('/webhook/facebook', (req, res) => {
     console.log('request recived');
 
-     
-  let body = req.body;
 
-  // Checks this is an event from a page subscription
-  if (body.object === 'page') {
+    let body = req.body;
 
-    // Iterates over each entry - there may be multiple if batched
-    body.entry.forEach(function(entry) {
+    // Checks this is an event from a page subscription
+    if (body.object === 'page') {
 
-      // Gets the message. entry.messaging is an array, but 
-      // will only ever contain one message, so we get index 0
-      let webhook_event = entry.messaging[0];
-      console.log(webhook_event);
-      const id = webhook_event.sender.id
-      const message = webhook_event.message.text;
-      if (message) {
-        sendFacebookMessage(id, "Thank you for messaging us");
-      }
-    });
+        // Iterates over each entry - there may be multiple if batched
+        body.entry.forEach(function (entry) {
 
-    // Returns a '200 OK' response to all requests
-    res.status(200).send('EVENT_RECEIVED');
-  } else {
-    // Returns a '404 Not Found' if event is not from a page subscription
-    res.status(404).send('Invalid request');
-  }
+            // Gets the message. entry.messaging is an array, but 
+            // will only ever contain one message, so we get index 0
+            let webhook_event = entry.messaging[0];
+            console.log(webhook_event);
+            console.log('ID:' + webhook_event.sender.id + ' Text: ' + webhook_event.message.text);
+
+            sendFacebookMessage('314676126112441', "Thank you for messaging us");
+
+        });
+
+        // Returns a '200 OK' response to all requests
+        res.status(200).send('EVENT_RECEIVED');
+    } else {
+        // Returns a '404 Not Found' if event is not from a page subscription
+        res.status(404).send('Invalid request');
+    }
 });
 
 module.exports = router;
