c873ccc0aa986b726d724b2fc0c98bd849640302
nurulhuda859 <nurulhuda859g@gmail.com>
2019-06-04 20:32:41 +0600

    test

--- routes/callback.js
+++ routes/callback.js
@@ -47,10 +47,10 @@ router.post('/webhook/facebook', (req, res) => {
             // Gets the message. entry.messaging is an array, but 
             // will only ever contain one message, so we get index 0
             let webhook_event = entry.messaging[0];
-            console.log(webhook_event);
-            // console.log('ID:' + webhook_event.sender.id + ' Text: ' + webhook_event.message.text); 
+            // console.log(webhook_event);
+            console.log('ID:' + webhook_event.sender); 
         });
-        sendFacebookMessage('2207807745965356', "Thank you for messaging us");
+        // sendFacebookMessage('2207807745965356', "Thank you for messaging us");
         // Returns a '200 OK' response to all requests
         res.status(200).send('EVENT_RECEIVED');
     } else {
